ec36b9ab6e5b38bfe702bb215bfe7ef9b4ec3f9d
Alissongdb <alisson.gabriel5@hotmail.com>
2024-11-23 18:05:04 -0300

    commit final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'package:moveis_api_v2/movies_list.dart';
+import 'package:moveis_api_v2/movies_list_error.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +18,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  MoviesDB? movies;
+  MoviesListError? moviesListError;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -21,23 +28,37 @@ class _MyAppState extends State<MyApp> {
     _getListagemAPI();
   }
 
-  void _getListagemAPI() {
+  Future<void> _getListagemAPI() async {
     http.get(
-      Uri.https('api.themoviedb.org', '/4/list/1'),
+      Uri.https('api.themoviedb.org', '/4/list/3'),
       headers: {
         'authorization':
             'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTcwNWVjY2EyYmU2OWQyMmMyZGRhYjQxOTMzYmY4OCIsIm5iZiI6MTczMjM4ODY5MS4xNzEyNjczLCJzdWIiOiI2NWIxOWE1MTBmMmZiZDAxNGE2NjUzOTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.bVTdQ9G4l1baZF-6sJ6Zbn4UzsOmYAK0rMYU9fYQ-0Q',
         'content-type': 'application/json;charset-utf-8'
       },
-    ).then((Response value) => print(value.body));
+    ).then(
+      (Response value) {
+        if (value.statusCode == 200) {
+          movies = MoviesDB.fromJson(
+            jsonDecode(value.body),
+          );
+        } else if ([401, 404, 500].contains(value.statusCode)) {
+          moviesListError = MoviesListError.fromJson(
+            jsonDecode(value.body),
+          );
+        }
+      },
+    ).whenComplete(() => setState(() {}));
   }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World'),
+          child: moviesListError != null
+              ? Text(moviesListError.toString())
+              : Text('Movie: ${movies?.name ?? "Erro"}'),
         ),
       ),
     );
